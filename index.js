e22d738605d48894eaf00934014d510c0887838b
DhanushM12 <dhanushmukhi@gmail.com>
2022-04-05 22:00:30 +0530

    added all routes

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ if(process.env.NODE_ENV !== 'production'){
 }
 
 
-
 const express = require('express');
 const app = express();
 const port = 8000;
 const passport = require('passport');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
-const flash = require('express-flash');
+// const flash = require('express-flash');
+const methodOveride = require('method-override');
 const users = [];
 
 const initialize = require('./config/passportlocal');
@@ -30,18 +30,51 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(methodOveride('_method'))
+
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', {name: req.user.name})
+})
+
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login.ejs')
 })
 
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}))
+
 app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs')
 })
 
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+    try {
+        const hashpasword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashpasword
+        })
+        res.redirect('/login');
+    } catch (error) {
+        console.log(error)
+        res.redirect('/register');
+    }
+})
+
+app.delete('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/login');
+})
+
 function checkAuthenticated(req, res, next){
     if(req.isAuthenticated()){
-        next();
+        return next();
     }
     return res.redirect('/login')
 }
@@ -53,6 +86,8 @@ function checkNotAuthenticated(req, res, next){
     }
     next();
 }
+
+console.log(users);
 app.listen(port, function(err){
     if(err){
         console.log(`Error i running the server : ${err}`);
